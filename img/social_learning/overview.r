1a5a81fbe07fa6439a499cb6a8adf630c4b90238
megabyte22 <a.l.w.kuijper@exeter.ac.uk>
2019-08-19 08:49:05 +0100

    added social learning in response to variation in p to the graphs

--- img/social_learning/overview.r
+++ img/social_learning/overview.r
@@ -26,7 +26,7 @@ print(
       )
 dev.off()
 
-dat <- read.table("../../data/summary_vary_qmat.csv"
+dat <- read.table("../../data/summary_social_learn_qmat.csv"
                   ,sep=";"
                   ,header=T)
 
@@ -34,6 +34,31 @@ pdf("overview_a_vary_qmat.pdf")
 print(
       xyplot(mean_agen + mean_asoc + mean_ajuv + mean_amat ~ qmat | p * qjuv * m * nc * np
              ,data=dat[dat$qmat >= 0.5,]
+             ,pch=21
+             ,cex=0.5
+             ,strip=function(strip.levels,...) { strip.default(strip.levels=T,...) }
+             ,auto.key=T)
+      )
+dev.off()
+
+dat <- read.table("../../data/summary_vary_p_social.csv"
+                  ,sep=";"
+                  ,header=T)
+
+pdf("overview_a_vary_p.pdf")
+print(
+      xyplot(mean_agen + mean_asoc + mean_ajuv + mean_amat ~ p| qmat * qjuv * m * nc * np
+             ,data=dat
+             ,pch=21
+             ,cex=0.5
+             ,strip=function(strip.levels,...) { strip.default(strip.levels=T,...) }
+             ,auto.key=T)
+      )
+dev.off()
+pdf("overview_dp_dc_vary_p.pdf")
+print(
+      xyplot(mean_dc + mean_dp ~ p| qmat * qjuv * m * nc * np
+             ,data=dat
              ,pch=21
              ,cex=0.5
              ,strip=function(strip.levels,...) { strip.default(strip.levels=T,...) }
