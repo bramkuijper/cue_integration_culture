a01afc083a9559a0c3ad7c2929e340d6cb0c3eb1
megabyte22 <a.l.w.kuijper@exeter.ac.uk>
2019-12-09 23:50:20 +0000

    figure 3 L&M redone, sort of...

--- src/ibm/generate_params.py
+++ src/ibm/generate_params.py
@@ -15,12 +15,12 @@ sigmoidal_survival = [ 0 ]
 # frequency of the high environment
 #p = list(np.linspace(0,1,30))
 #p = [ 0.05, 0.1,0.25, 0.5, 0.75, 0.9, 0.95, 0.99 ]
-p = [ 0.1, 0.9 ]
+p = [ 0.82 ]
 
 survival_scalar_sig = [-2.5,3.5]
 survival_scalar_quad = [0.8,0.0]
 
-qmat = [0.9]
+qmat = [1.0]
 qjuv = [0.5]
 
 nloci_g = [ 3 ]
@@ -46,8 +46,8 @@ sdsoc_vert = [ 0.05 ]
 m = [0.1]
 
 # mu_g, mu_amat, mu_ajuv, mu_agen, mu_asoc_horiz, mu_asoc_vert, mu_bmat_phen, mu_bmat_envt, mu_hp, mu_hc, mu_vp, mu_vc
-mu_combis = [[ 0.01, 0.01, 0.01, 0.01, 0, 0, 0.01, 0.01, 0, 0, 0, 0 ]]#[ [ 0.01 for x in range(0,12) ]]
-#mu_combis = [ [ 0.01 for x in range(0,12) ]]
+#mu_combis = [[ 0.01, 0.01, 0.01, 0.01, 0, 0, 0.01, 0.01, 0, 0, 0, 0 ]]#[ [ 0.01 for x in range(0,12) ]]
+mu_combis = [ [ 0.01 for x in range(0,12) ]]
 
 #mu_combis = [[ 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001 ]]
 sdmu = "0.02 0.25 0.25"
@@ -63,7 +63,7 @@ ctr = 1
 # whether jobs should be run in the background
 run_in_background = True
 
-juvenile_survival = [ 0 ]
+juvenile_survival = [ 1 ]
 
 # never run background jobs on cluster
 hostname = socket.gethostname()
